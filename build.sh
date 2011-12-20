f22bbeca97cab46743f9b50ad8eda4c46540ca3b
poitee <lynchdc39987@gmail.com>
2011-12-19 20:46:06 -0500

    Build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+make ARCH=arm -j4 CROSS_COMPILE=~/android/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+mv ~/kernel/omap/arch/arm/boot/zImage ~/kernel/anykernel/kernel/
+cd ~/kernel/anykernel
+ZIP="GnexKernel-`date +%m%d`.zip"
+zip -r $ZIP .
+mv *.zip /~
+rm ./kernel/zImage
